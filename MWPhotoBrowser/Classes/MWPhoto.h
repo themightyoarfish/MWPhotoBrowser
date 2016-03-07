8bdb9d5c98fd4aaa89230d077464d5d116d2eed1
themightyoarfish <rdiederichse@uos.de>
2016-03-07 10:55:47 +0100

    Fixed some deprecation warnings, started transforming mwphoto into mwrephoto

--- MWPhotoBrowser/Classes/MWPhoto.h
+++ MWPhotoBrowser/Classes/MWPhoto.h
@@ -16,17 +16,20 @@
 @interface MWPhoto : NSObject <MWPhoto>
 
 @property (nonatomic, strong) NSString *caption;
-@property (nonatomic, readonly) UIImage *image;
-@property (nonatomic, readonly) NSURL *photoURL;
-@property (nonatomic, readonly) NSString *filePath  __attribute__((deprecated("Use photoURL"))); // Depreciated
+@property (nonatomic, readonly) UIImage *beforeImage;
+@property (nonatomic, readonly) UIImage *afterImage;
+@property (nonatomic, readonly) NSURL *beforeURL;
+@property (nonatomic, readonly) NSURL *afterURL;
+@property (nonatomic, readonly) NSString *beforeFilePath  __attribute__((deprecated("Use photoURL"))); // Depreciated
+@property (nonatomic, readonly) NSString *afterFilePath  __attribute__((deprecated("Use photoURL"))); // Depreciated
 
-+ (MWPhoto *)photoWithImage:(UIImage *)image;
-+ (MWPhoto *)photoWithFilePath:(NSString *)path  __attribute__((deprecated("Use photoWithURL: with a file URL"))); // Depreciated
-+ (MWPhoto *)photoWithURL:(NSURL *)url;
++ (MWPhoto *)photoWithImage:(UIImage *)image1 image2:(UIImage*)image2;
++ (MWPhoto *)photoWithFilePath:(NSString *)path1 path2:(NSString*)path2  __attribute__((deprecated("Use photoWithURL: with a file URL"))); // Depreciated
++ (MWPhoto *)photoWithURL:(NSURL *)url1 url2:(NSURL *)url2;
 
-- (id)initWithImage:(UIImage *)image;
-- (id)initWithURL:(NSURL *)url;
-- (id)initWithFilePath:(NSString *)path  __attribute__((deprecated("Use initWithURL: with a file URL"))); // Depreciated
+- (id)initWithImage:(UIImage *)image1 image2:(UIImage *)image2;
+- (id)initWithURL:(NSURL *)url1 url2:(NSURL *)url2;
+- (id)initWithFilePath:(NSString *)path1 path2:(NSString *)path2 __attribute__((deprecated("Use initWithURL: with a file URL"))); // Depreciated
 
 @end
 
